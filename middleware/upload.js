4bcb19ebb1bdc72ec49909341d69333d8befdc49
Moustafa Mohamed <mostafa_msamir@hotmail.com>
2025-09-18 18:47:40 +0300

    Update upload.js

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,5 +1,11 @@
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
+const uploadPath = path.join(__dirname, "../uploads/questions");
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
+}
+
 
 // Storage config
 const storage = multer.diskStorage({
