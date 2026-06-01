3b3e3c1e024dfe9184be70690937a7096a8ecef2
Moustafa Mohamed <111659887+Darsh2984@users.noreply.github.com>
2026-06-01 20:14:30 +0300

    auth update

--- routes/auth.js
+++ routes/auth.js
@@ -118,8 +118,8 @@ router.post("/login", async (req, res) => {
       typedPassword: password,
       storedHash: user.password,
     });
-    // const isMatch = await bcrypt.compare(password, user.password);
-    // if (!isMatch) return res.status(400).json({ msg: "❌ Invalid credentials" });
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: "❌ Invalid credentials" });
 
     // after successful login
     if (user.role === "student") {
